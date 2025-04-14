3c9809007a5bf66de71de72ca526019d23ef1904
Himanshi project <himanshikhatri90@gmail.com>
2025-04-14 14:51:37 +0530

    this is my introduction

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hi, I'm Himanshi Khatri, a passionate Computer Science student at SKIT.\n");
+    printf("I love working on DSA, JavaScript, and Python projects.\n");
+    printf("Currently, I'm exploring GitHub and learning through CodeHelp and LeetCode.\n");
+    printf("I'm also part of the Wisflux ILP program to expand my tech knowledge.\n");
+    printf("Connect with me on GitHub: github.com/123himanshi-khatri\n");
+    return 0;
+}
